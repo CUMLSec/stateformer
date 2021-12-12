87cb3c906c43fcff42b2ca820eb6e7fd918d0a1c
Kexin Pei <peikexin9@gmail.com>
2021-12-11 22:10:16 -0500

    add function level prediction for function signature

---
+++ command/finetune/finetune_signature.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+CHECKPOINT_PATH=checkpoints/finetune_signature
+mkdir -p $CHECKPOINT_PATH
+rm -f $CHECKPOINT_PATH/checkpoint_best.pt
+cp checkpoints/pretrain/checkpoint_best.pt $CHECKPOINT_PATH/
+
+TOTAL_UPDATES=6000    # Total number of training steps
+WARMUP_UPDATES=100    # Warmup the learning rate over this many updates
+PEAK_LR=1e-5          # Peak learning rate, adjust as needed
+TOKENS_PER_SAMPLE=512 # Max sequence length
+MAX_POSITIONS=512     # Num. positional embeddings (usually same as above)
+MAX_SENTENCES=8       # Number of sequences per batch (batch size)
+NUM_CLASSES=7         # int|char|float|void∗|enum|union|struct
+ENCODER_EMB_DIM=768
+ENCODER_LAYERS=8
+ENCODER_ATTENTION_HEADS=12
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+  data-bin/finetune/signature \
+  --num-classes $NUM_CLASSES \
+  --task signature --criterion signature --arch roberta_mf_nau \
+  --reset-optimizer --reset-dataloader --reset-meters \
+  --optimizer adam --adam-betas '(0.9,0.98)' --adam-eps 1e-6 --clip-norm 0.0 \
+  --lr-scheduler polynomial_decay --lr $PEAK_LR --warmup-updates $WARMUP_UPDATES --total-num-update $TOTAL_UPDATES \
+  --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+  --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric \
+  --encoder-layers $ENCODER_LAYERS --encoder-embed-dim $ENCODER_EMB_DIM --encoder-attention-heads $ENCODER_ATTENTION_HEADS \
+  --max-positions 512 --max-sentences $MAX_SENTENCES --update-freq 4 \
+  --max-update $TOTAL_UPDATES --log-format json --log-interval 10 \
+  --no-epoch-checkpoints --save-dir $CHECKPOINT_PATH/ \
+  --memory-efficient-fp16 \
+  --restore-file $CHECKPOINT_PATH/checkpoint_best.pt |
+  tee result/finetune_signature
---
+++ fairseq/tasks/signature.py
@@ -0,0 +1,222 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+import os
+
+import numpy as np
+
+from fairseq.data import (
+    data_utils,
+    Dictionary,
+    IdDataset,
+    OffsetTokensDataset,
+    NestedDictionaryDataset,
+    NumelDataset,
+    NumSamplesDataset,
+    RightPadDataset,
+    PrependTokenDataset,
+    SortDataset,
+    StripTokenDataset,
+    TruncateDataset
+)
+from fairseq.tasks import register_task, LegacyFairseqTask
+from fairseq.data.shorten_dataset import maybe_shorten_dataset
+from fairseq import utils
+from command import params
+
+logger = logging.getLogger(__name__)
+
+
+@register_task('signature')
+class SignatureMF(LegacyFairseqTask):
+    """Task for training masked language models (e.g., BERT, RoBERTa)."""
+
+    @staticmethod
+    def add_args(parser):
+        """Add task-specific arguments to the parser."""
+        parser.add_argument('data', help='colon separated path to data directories list, \
+                            will be iterated upon during epochs in round-robin manner')
+        parser.add_argument('--num-classes', type=int, default=2,
+                            help='number of classes')
+        parser.add_argument('--no-shuffle', action='store_true', default=False)
+
+    def __init__(self, args, data_dictionary_dict, label_dictionary, dictionary_cf):
+        super().__init__(args)
+        self.dictionary_dict = data_dictionary_dict
+        self.dictionary_cf = dictionary_cf
+        self._label_dictionary = label_dictionary
+        if not hasattr(args, 'max_positions'):
+            self._max_positions = (
+                args.max_source_positions,
+                args.max_target_positions,
+            )
+        else:
+            self._max_positions = args.max_positions
+        args.tokens_per_sample = self._max_positions
+
+        self.seed = args.seed
+
+        # add mask token
+        # self.mask_idx_dict = {}
+        # for field, dictionary in dictionary_dict.items():
+        #     self.mask_idx_dict[field] = dictionary.add_symbol('<mask>')
+
+        # All field of each token
+        self.fields = params.fields
+
+    @classmethod
+    def load_dictionary(cls, args, filename, source=True):
+        """Load the dictionary from the filename
+
+        Args:
+            filename (str): the filename
+        """
+        dictionary = Dictionary.load(filename)
+        dictionary.add_symbol('<mask>')
+        return dictionary
+
+    @classmethod
+    def setup_task(cls, args, **kwargs):
+        # paths = utils.split_paths(args.data)
+        # paths = os.listdir(args.data)
+        # assert len(paths) > 0
+        # assert len(paths) == len(params.fields)
+        assert args.num_classes > 0, 'Must set --num-classes'
+
+        data_dictionary_dict = {}
+        for field in params.fields:
+            data_dictionary_dict[field] = cls.load_dictionary(
+                args,
+                os.path.join(args.data, field, 'dict.txt'),
+                source=True
+            )
+            logger.info(f'| [input] {field} dictionary: {len(data_dictionary_dict[field])} types')
+
+        # load label dictionary
+        label_dict = cls.load_dictionary(
+            args,
+            os.path.join(args.data, 'label', 'dict.txt'),
+            source=False,
+        )
+        print('| [label] dictionary: {} types'.format(len(label_dict)))
+
+        # control flow label
+        dictionary_cf = Dictionary.load(os.path.join(args.data, params.field_cf, 'dict.txt'))
+        logger.info(f'{params.field_cf} dictionary: {len(dictionary_cf)} types')
+
+        return cls(args, data_dictionary_dict, label_dict, dictionary_cf)
+
+    def load_dataset(self, split, epoch=1, combine=False, **kwargs):
+        """Load a given dataset split.
+
+        Args:
+            split (str): name of the split (e.g., train, valid, test)
+        """
+        paths = utils.split_paths(self.args.data)
+        assert len(paths) > 0
+        data_path = paths[(epoch - 1) % len(paths)]
+
+        src_tokens = {}
+        target = {}
+        for field in self.fields:
+            split_path = os.path.join(self.args.data, field, split)
+
+            src_dataset = data_utils.load_indexed_dataset(
+                split_path,
+                self.source_dictionary[field],
+                self.args.dataset_impl,
+                combine=combine,
+            )
+            if src_dataset is None:
+                raise FileNotFoundError('Dataset not found: {} ({})'.format(split, split_path))
+
+            src_dataset = PrependTokenDataset(src_dataset, self.source_dictionary[field].bos())
+            src_tokens[field] = RightPadDataset(
+                TruncateDataset(src_dataset, self.args.max_positions),
+                pad_idx=self.source_dictionary[field].pad())
+
+        net_input = dict()
+        net_input['src_tokens'] = src_tokens
+        net_input['src_lengths'] = NumelDataset(src_dataset, reduce=False)
+
+        with data_utils.numpy_seed(self.args.seed + epoch):
+            shuffle = np.random.permutation(len(src_dataset))
+
+        # Net input has multiple fields
+        dataset = {
+            'id': IdDataset(),
+            'net_input': net_input,
+            'target': target,
+            'nsentences': NumSamplesDataset(),
+            'ntokens': NumelDataset(src_dataset, reduce=True),
+        }
+
+        label_path = os.path.join(self.args.data, 'label', split)
+        label_dataset = data_utils.load_indexed_dataset(
+            label_path,
+            self.label_dictionary,
+            self.args.dataset_impl,
+            combine=combine,
+        )
+
+        if label_dataset is None:
+            raise FileNotFoundError('Dataset not found: {} ({})'.format(split, label_path))
+
+        dataset.update(
+            target=OffsetTokensDataset(
+                StripTokenDataset(
+                    label_dataset,
+                    id_to_strip=self.label_dictionary.eos()),
+                offset=-self.label_dictionary.nspecial,
+            )
+        )
+
+        nested_dataset = NestedDictionaryDataset(
+            dataset,
+            sizes=[src_dataset.sizes],
+        )
+
+        if self.args.no_shuffle:
+            self.datasets[split] = nested_dataset
+        else:
+            self.datasets[split] = SortDataset(
+                nested_dataset,
+                # shuffle
+                sort_order=[shuffle],
+            )
+
+        logger.info("Loaded {0} with #samples: {1}".format(split, len(self.datasets[split])))
+        return self.datasets[split]
+
+    def build_model(self, args):
+        from fairseq import models
+        model = models.build_model(args, self)
+
+        model.register_classification_head(
+            getattr(args, 'classification_head_name', 'signature_head'),
+            num_classes=self.args.num_classes,
+        )
+
+        return model
+
+    def max_positions(self):
+        return self._max_positions
+
+    @property
+    def source_dictionary(self):
+        return self.dictionary_dict
+
+    @property
+    def target_dictionary(self):
+        return self.dictionary_dict
+
+    @property
+    def label_dictionary(self):
+        return self._label_dictionary
+
+    @property
+    def target_cf_dictionary(self):
+        return self.dictionary_cf
